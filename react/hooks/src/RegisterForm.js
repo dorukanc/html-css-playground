234cb50eae8feb0bfbd595bd126b30ea4d7f4b6d
dorukanc <catakd@mef.edu.tr>
2023-11-13 05:52:25 +0300

    e was not defined, thus updated

--- react/hooks/src/RegisterForm.js
+++ react/hooks/src/RegisterForm.js
@@ -11,15 +11,15 @@ export default function RegisterForm(){
         <>
             <label>
                 Fist name:
-                <input value={form.firstName} onChange={setForm({...form, firstName: e.target.value})}/>
+                <input value={form.firstName} onChange={e => {setForm({...form, firstName: e.target.value})}}/>
             </label>
             <label>
                 Last name:
-                <input value={form.lastName} onChange={setForm({...form, lastName: e.target.value})}/>
+                <input value={form.lastName} onChange={e => {setForm({...form, lastName: e.target.value})}}/>
             </label>
             <label>
                 Email:
-                <input value={form.email} onChange={setForm({...form, email: e.target.value})}/>
+                <input value={form.email} onChange={e => {setForm({...form, email: e.target.value})}}/>
             </label>
             <p>
                 {form.firstName}{' '}
